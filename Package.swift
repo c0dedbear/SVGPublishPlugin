c904bdbfd898e4750e4a2c1330827edc1ccc1454
Mikhail Medvedev <icodedbear@gmail.com>
2023-05-06 20:35:40 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
 		.package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.8.0"),
-		.package(url: "https://github.com/c0dedbear/StrongTypedCSSPublishPlugin", from: "0.1.0")
+		.package(url: "https://github.com/c0dedbear/StrongTypedCSSPublishPlugin", from: "0.1.1")
     ],
     targets: [
 		.target(
